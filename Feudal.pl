bb322775850c7d505eb9dec0cfeac69743a35250
miaosun88 <miaosun88@aaef916e-e55c-526c-4b85-800ccecf3d75>
2012-10-07 16:37:59 +0000

    alteração na representação do tabuleiro  '##' em vez de 'XX', '**' em vez de 'TT', para nao complicar o tabuleiro. adicionei mais um estado com todas as peças colocadas no tabuleiro para efeito de testar a visualização do tabuleiro.

--- Feudal.pl
+++ Feudal.pl
@@ -11,20 +11,38 @@
 % t - rough terrain
 % 0 - vazia
 
-%% Peças do jogo
-% c - Castle    x1
-% g - Green     x1
-% 1 - King      x1
-% 2 - Prince    x1
-% 3 - Duke      x1
-% 4 - Knights   x2
-% 5 - Sergeants x2
-% 6 - Squire    x1
-% 7 - Archer    x1
-% 8 - Pikemen   x4
+%%%%%%%%%% Peças do jogo
+%%%Jogador 1
+% ac - Castle    x1
+% ag - Green     x1
+% a1 - King      x1
+% a2 - Prince    x1
+% a3 - Duke      x1
+% a4 - Knights   x2
+% a5 - Sergeants x2
+% a6 - Squire    x1
+% a7 - Archer    x1
+% a8 - Pikemen   x4
+
+%%%Jogador 2
+% bc - Castle    x1
+% bg - Green     x1
+% b1 - King      x1
+% b2 - Prince    x1
+% b3 - Duke      x1
+% b4 - Knights   x2
+% b5 - Sergeants x2
+% b6 - Squire    x1
+% b7 - Archer    x1
+% b8 - Pikemen   x4
+
 
 mountedmen([2,3,4,4]).
 footmen([1,5,5,6,7,8,8,8,8]).
+
+jogador(j1,1).
+jogador(j2,2).
+
 
 %%%%%%%%  Estado inicial  %%%%%%%%
 estadoInicial([[x,0,0,0,0,0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,0,0,x],
@@ -53,7 +71,7 @@ estadoInicial([[x,0,0,0,0,0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,0,0,x],
                [x,0,0,0,0,t,x,0,0,0,0,0,0,0,0,0,0,x,x,0,0,0,0,x]]).
 
 
-show:-estadoInicial(X), print_tab(X).
+show:-estadoTeste(X), print_tab(X).
 
 print_tab(Tab):- writeln('    A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X '),
 		 printLists(Tab,1),
@@ -85,9 +103,29 @@ printList([FElem|OElem]):-
                         printList(OElem).
 
 draw_casa(Elem):-  %desenha a casa com os valores respectivmente
-	Elem == 0, write('  ');
-	Elem == x, write('XX');
-	Elem == t, write('TT').
+	Elem == 0,  write('  ');
+	Elem == x,  write('##');
+	Elem == t,  write('**');
+	Elem == ac, write('1C');
+	Elem == ag, write('1G');
+	Elem == a1, write('1K');
+	Elem == a2, write('1P');
+	Elem == a3, write('1D');
+	Elem == a4, write('1k');
+	Elem == a5, write('1S');
+	Elem == a6, write('1s');
+	Elem == a7, write('1A');
+	Elem == a8, write('1p');
+	Elem == bc, write('2C');
+	Elem == bg, write('2G');
+	Elem == b1, write('2K');
+	Elem == b2, write('2P');
+	Elem == b3, write('2D');
+	Elem == b4, write('2k');
+	Elem == b5, write('2S');
+	Elem == b6, write('2s');
+	Elem == b7, write('2A');
+	Elem == b8, write('2p').
 
 
 start:-
@@ -150,3 +188,34 @@ menu_nivel:-
         writeln('**********************************'),nl,
 	write('faca a sua escolha: '), faz_opcao(Op),
 	write(Op), integer(Op),writeln(' um numero').
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%% estado para testar tabuleiro %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+estadoTeste([[x,0,0,0,0,0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,0,0,x],
+	     [0,0,0,0,0,0,0,0,0,0,0,0,0,x,x,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [t,0,0,0,0,0,0,0,0,0,x,0,0,0,0,t,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,x,0,0,0,x,0,0,0,t,0,0,0,0,0,x,0,0,x],
+             [0,0,0,t,0,a2,t,x,a7,0,0,0,0,0,0,0,x,x,x,0,x,t,0,x],
+             [0,0,0,x,0,a1,ag,ac,x,0,0,0,0,0,0,0,0,0,0,x,0,0,0,0],
+             [0,0,0,0,0,a3,0,x,x,a8,0,0,0,0,0,0,x,0,0,x,x,0,0,0],
+             [0,0,0,0,0,0,x,x,0,a4,a5,0,a8,a8,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,x,0,0,a4,a5,0,a8,x,x,0,0,0,0,0,0,0,0,0],
+             [0,x,0,0,x,t,0,0,0,x,0,a6,0,0,x,x,t,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,x,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,x,0,0,0,t,x,0,0,t,x,x,0,0,0],
+             [0,0,0,0,t,x,0,0,0,0,0,0,0,0,0,b8,b8,b8,b6,0,x,0,0,0],
+             [0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,b5,b4,b8,b7,0,0,0,0],
+             [t,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,b4,x,x,t,b1,0,0,0],
+             [x,x,0,0,0,x,x,0,0,0,0,0,0,0,0,0,b5,x,bg,bc,b2,0,0,0],
+             [0,0,0,0,0,t,x,0,0,0,0,0,0,0,0,x,x,x,x,0,b3,0,0,0],
+             [0,0,0,0,0,x,0,x,0,0,x,0,0,x,0,0,0,t,0,0,0,0,0,x],
+             [0,0,0,0,x,x,0,0,0,x,x,0,0,t,0,0,x,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+             [x,0,0,0,0,t,x,0,0,0,0,0,0,0,0,0,0,x,x,0,0,0,0,x]]).
