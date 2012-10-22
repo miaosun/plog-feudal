453b3149e04e1ffa9416f2cad8a9606a0fe1dadd
miaosun88 <miaosun88@aaef916e-e55c-526c-4b85-800ccecf3d75>
2012-10-22 10:18:37 +0000

    alteração das peças

--- trunk/Feudal.pl
+++ trunk/Feudal.pl
@@ -13,32 +13,35 @@
 
 %%%%%%%%%% Peças do jogo
 %%%Jogador 1
-% ac - Castle    x1
-% ag - Green     x1
-% a1 - King      x1
-% a2 - Prince    x1
-% a3 - Duke      x1
-% a4 - Knights   x2
-% a5 - Sergeants x2
-% a6 - Squire    x1
-% a7 - Archer    x1
-% a8 - Pikemen   x4
+% aC - Castle    x1
+% aG - Green     x1
+% aK - King      x1
+% aP - Prince    x1
+% aD - Duke      x1
+% ak - Knights   x2
+% aS - Sergeants x2
+% as - Squire    x1
+% aa - Archer    x1
+% ap - Pikemen   x4
 
 %%%Jogador 2
-% bc - Castle    x1
-% bg - Green     x1
-% b1 - King      x1
-% b2 - Prince    x1
-% b3 - Duke      x1
-% b4 - Knights   x2
-% b5 - Sergeants x2
-% b6 - Squire    x1
-% b7 - Archer    x1
-% b8 - Pikemen   x4
+% bC - Castle    x1
+% bG - Green     x1
+% bK - King      x1
+% bP - Prince    x1
+% bD - Duke      x1
+% bK - Knights   x2
+% bS - Sergeants x2
+% bs - Squire    x1
+% ba - Archer    x1
+% bp - Pikemen   x4
 
 
-mountedmen([2,3,4,4]).
-footmen([1,5,5,6,7,8,8,8,8]).
+mountedmen([aP,aD,ak, bP,bD,bk]).
+footmen([aK,aS,as,aa,ap, bK,bS,bs,ba,bp]).
+peca_J1([aC,aG,aK,aP,aD, ak,ak,aS,aS,as,aa,ap,ap,ap,ap]).
+peca_J2([bC,bG,bK,bP,bD, bk,bk,bS,bS,bs,ba,bp,bp,bp,bp]).
+
 
 jogador(j1,1).
 jogador(j2,2).
@@ -108,26 +111,26 @@ draw_casa(Elem):-  %desenha a casa com os valores respectivmente
 	Elem == 0,  write('  ');
 	Elem == x,  write('##');
 	Elem == t,  write('**');
-	Elem == ac, write('1C');
-	Elem == ag, write('1G');
-	Elem == a1, write('1K');
-	Elem == a2, write('1P');
-	Elem == a3, write('1D');
-	Elem == a4, write('1k');
-	Elem == a5, write('1S');
-	Elem == a6, write('1s');
-	Elem == a7, write('1A');
-	Elem == a8, write('1p');
-	Elem == bc, write('2C');
-	Elem == bg, write('2G');
-	Elem == b1, write('2K');
-	Elem == b2, write('2P');
-	Elem == b3, write('2D');
-	Elem == b4, write('2k');
-	Elem == b5, write('2S');
-	Elem == b6, write('2s');
-	Elem == b7, write('2A');
-	Elem == b8, write('2p').
+	Elem == aC, write('1C');
+	Elem == aG, write('1G');
+	Elem == aK, write('1K');
+	Elem == aP, write('1P');
+	Elem == aD, write('1D');
+	Elem == ak, write('1k');
+	Elem == aS, write('1S');
+	Elem == as, write('1s');
+	Elem == aa, write('1a');
+	Elem == ap, write('1p');
+	Elem == bC, write('2C');
+	Elem == bG, write('2G');
+	Elem == bK, write('2K');
+	Elem == bP, write('2P');
+	Elem == bD, write('2D');
+	Elem == bk, write('2k');
+	Elem == bS, write('2S');
+	Elem == bs, write('2s');
+	Elem == ba, write('2a');
+	Elem == bp, write('2p').
 
 
 start:-
@@ -210,20 +213,20 @@ estadoTeste([[x,0,0,0,0,0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,0,0,x],
              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
              [t,0,0,0,0,0,0,0,0,0,x,0,0,0,0,t,0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,x,0,0,0,x,0,0,0,t,0,0,0,0,0,x,0,0,x],
-             [0,0,0,t,0,a2,t,x,a7,0,0,0,0,0,0,0,x,x,x,0,x,t,0,x],
-             [0,0,0,x,0,a1,ag,ac,x,0,0,0,0,0,0,0,0,0,0,x,0,0,0,0],
-             [0,0,0,0,0,a3,0,x,x,a8,0,0,0,0,0,0,x,0,0,x,x,0,0,0],
-             [0,0,0,0,0,0,x,x,0,a4,a5,0,a8,a8,0,0,0,0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,x,0,0,a4,a5,0,a8,x,x,0,0,0,0,0,0,0,0,0],
-             [0,x,0,0,x,t,0,0,0,x,0,a6,0,0,x,x,t,0,0,0,0,0,0,0],
+             [0,0,0,t,0,aP,t,x,aa,0,0,0,0,0,0,0,x,x,x,0,x,t,0,x],
+             [0,0,0,x,0,aK,aG,aC,x,0,0,0,0,0,0,0,0,0,0,x,0,0,0,0],
+             [0,0,0,0,0,aD,0,x,x,ap,0,0,0,0,0,0,x,0,0,x,x,0,0,0],
+             [0,0,0,0,0,0,x,x,0,ak,aS,0,ap,ap,0,0,0,0,0,0,0,0,0,0],
+             [0,0,0,0,0,0,x,0,0,ak,aS,0,ap,x,x,0,0,0,0,0,0,0,0,0],
+             [0,x,0,0,x,t,0,0,0,x,0,as,0,0,x,x,t,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0,x,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0,0,x,0,0,0,t,x,0,0,t,x,x,0,0,0],
-             [0,0,0,0,t,x,0,0,0,0,0,0,0,0,0,b8,b8,b8,b6,0,x,0,0,0],
-             [0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,b5,b4,b8,b7,0,0,0,0],
-             [t,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,b4,x,x,t,b1,0,0,0],
-             [x,x,0,0,0,x,x,0,0,0,0,0,0,0,0,0,b5,x,bg,bc,b2,0,0,0],
-             [0,0,0,0,0,t,x,0,0,0,0,0,0,0,0,x,x,x,x,0,b3,0,0,0],
+             [0,0,0,0,t,x,0,0,0,0,0,0,0,0,0,bp,bp,bp,bs,0,x,0,0,0],
+             [0,0,0,0,x,x,0,0,0,0,0,0,0,0,0,0,bS,bk,bp,ba,0,0,0,0],
+             [t,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,bk,x,x,t,bK,0,0,0],
+             [x,x,0,0,0,x,x,0,0,0,0,0,0,0,0,0,bS,x,bG,bC,bP,0,0,0],
+             [0,0,0,0,0,t,x,0,0,0,0,0,0,0,0,x,x,x,x,0,bD,0,0,0],
              [0,0,0,0,0,x,0,x,0,0,x,0,0,x,0,0,0,t,0,0,0,0,0,x],
              [0,0,0,0,x,x,0,0,0,x,x,0,0,t,0,0,x,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
